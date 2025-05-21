97518b6027e487eec89d2c267af6a76aab9d87d4
Tryphaxe <dibifondy02@gmail.com>
2025-05-21 23:37:19 +0000

    Fixed

--- src/app/FeaturePicTwo.js
+++ src/app/FeaturePicTwo.js
@@ -5,7 +5,7 @@ import Image from 'next/image';
 export default function FeaturePicTwo() {
 
 	return (
-		<div className="overflow-hidden bg-white py-24 sm:py-12">
+		<div className="overflow-hidden bg-blue-50 py-24 sm:py-12">
 			<div className="mx-auto max-w-7xl px-6 lg:px-8">
 				<div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
 					<Image
--- src/app/Hero.js
+++ src/app/Hero.js
@@ -3,22 +3,10 @@ import React from 'react';
 
 export default function Hero() {
 	return (
-		<div className="relative isolate px-0 sm:px-6 pt-5 lg:px-8 bg-white overflow-hidden h-[83vh] mx-0 sm:mx-12 mb-12 rounded-none sm:rounded-md">
-			<div
-				aria-hidden="true"
-				className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden sm:-top-80 sm:blur-md blur-none sm:block hidden"
-			>
-				<Image
-					src="/img/hdt.jpg"
-					width={1000}
-					height={1000}
-					className="h-full w-full object-cover object-center"
-					alt="Picture of the background"
-				/>
-			</div>
-			<div className="bg-[url('/img/hdt.jpg')] absolute -z-10 bg-cover bg-center h-full w-full sm:hidden block"/>
-			<div className="mx-auto max-w-2xl py-32 sm:py-12 lg:py-12 bg-[#0000006b] sm:bg-transparent">
-				<div className="hidden sm:mb-8 sm:flex sm:justify-center">
+		<div className="relative isolate bg-white overflow-hidden sm:h-[83vh] h-[100vh] mx-0 sm:mx-12 mb-12 rounded-none sm:rounded-md">
+			<div className="bg-[url('/img/trav.jpg')] absolute -z-10 bg-cover bg-center h-full w-full"/>
+			<div className=" h-full w-full py-32 sm:py-12 lg:py-12 bg-[#0000006b]">
+				<div className="hidden mx-auto max-w-2xl sm:mb-8 sm:flex sm:justify-center">
 					<div className="relative bg-white rounded-full px-3 py-1 text-sm/6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
 						Des opportunités uniques à l&apos;international.{' '}
 						<a href="#" className="font-semibold text-orange-600">
@@ -27,8 +15,8 @@ export default function Hero() {
 						</a>
 					</div>
 				</div>
-				<div className="text-center">
-					<h1 className="text-5xl font-semibold tracking-tight text-balance text-white sm:text-gray-900 sm:text-7xl">
+				<div className="text-center mx-auto max-w-2xl">
+					<h1 className="text-5xl font-semibold tracking-tight text-balance text-white sm:text-7xl">
 						Explorez le monde, atteignez vos ambitions.
 					</h1>
 					<p className="mt-8 text-lg font-medium text-pretty md:text-white text-gray-200 sm:text-xl/8">
@@ -37,11 +25,11 @@ export default function Hero() {
 					<div className="mt-10 flex items-center justify-center gap-x-6">
 						<a
 							href="#"
-							className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+							className="rounded-md bg-orange-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-orange-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-orange-600"
 						>
 							Inscription
 						</a>
-						<a href="#" className="text-sm/6 font-semibold sm:text-gray-900 text-white">
+						<a href="#" className="text-sm/6 font-semibold text-gray-200">
 							En savoir plus<span aria-hidden="true">→</span>
 						</a>
 					</div>
